e25a154faac31b32c2fcf4eaccf8d7b5efe6c5ac
strannik-j <strannik-j@mail.ru>
2013-06-19 14:38:15 +0400

    Update passgen.py
    New version

--- passgen.py
+++ passgen.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 #
 #  passgen.py
-#  
+#
 #  Copyright 2012 Strannik-j <mail@strannik-j.org>
-#  
+#
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
 #  met:
-#  
+#
 #  * Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 #  * Redistributions in binary form must reproduce the above
@@ -20,10 +20,10 @@
 #    this software without specific prior written permission.
 #  * All advertising materials mentioning features or use of this software
 #    must display the following acknowledgement:
-#  
+#
 #     This product includes software developed by a Strannik-j
 #     and his contributors.
-#  
+#
 #  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 #  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -40,178 +40,112 @@ import sys
 from tkinter import *
 from random import randrange
 
-root = Tk(screenName=None, className='PassGen')
-frame0 = Frame(root, width=100, height=100)
-
-
-ent_text0 = StringVar()
-ent_text1 = IntVar()
-
-
-var0 = IntVar()
-var1 = IntVar()
-var2 = IntVar()
-var3 = IntVar()
-
-var0.set(1)
-var1.set(1)
-var2.set(1)
-var3.set(0)
-ent_text0.set(6)
-ent_text1.set('')
-
-pass0 = StringVar()
-
-
-pa = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-pA = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-p0 = ('0','1','2','3','4','5','6','7','8','9')
-px = ('@','#','$','%','^','&','*','!',':',';','_','+','-','.',',','?')
-
-
-M = []
-
 
 def main():
-	ent_text0 = 5
-	return 0
+    x = sys.argv[1:]
+    t = []
+    try:
+        for i in x:
+            t.append(int(i))
+    except:
+        print('''Usage: passgen.py [a] [b] [c] [d]
+        a: quantity of characters (Default - 8)
+        b: usage "abc" (1 - yes, 0 - no; Default - 1)
+        c: usage "ABC" (1 - yes, 0 - no; Default - 1)
+        d: usage "123" (1 - yes, 0 - no; Default - 1)
+        e: usage "!@#" (1 - yes, 0 - no; Default - 0) \
+        ''')
+        sys.exit()
+
+    if len(t) == 0:
+        password = generate()
+    elif len(t) == 1:
+        password = generate(t[0])
+    elif len(t) == 2:
+        password = generate(t[0], t[1])
+    elif len(t) == 3:
+        password = generate(t[0], t[1], t[2])
+    elif len(t) == 4:
+        password = generate(t[0], t[1], t[2], t[3])
+    else:
+        password = generate(t[0], t[1], t[2], t[3], t[4])
+    print(password)
+    return 0
 
 
-def gen(event):
-	M = []
-	ch0 = 0
-	ch1 = int(ent_text0.get())
-	pass0 = ''
-	if var0.get() == 1:
-		M.extend(pa)
-	if var1.get() == 1:
-		M.extend(pA)
-	if var2.get() == 1:
-		M.extend(p0)
-	if var3.get() == 1:
-		M.extend(px)
-	lenM = len(M)
-	
-	
-	while ch0 <= ch1:
-		if ch0 == ch1:
+def generate(var_len=8, vpa=1, vpA=1, vp0=1, vpx=0):
+    pa = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+          'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    pA = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+          'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    p0 = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    px = ('@', '#', '$', '%', '^', '&', '*',
+          '!', ':', ';', '_', '+', '-', '.', ',', '?')
+    M = []
+    ch0 = 0
+    pass0 = ''
 
-			if var0.get() == 1 and ch1 > 1:
-				ch2 = 0
-				while ch2 < ch1:			
-					if pass0[ch2] in pa:
-						print("pa - совпадение")	 
-						ch2 += ch1*2
-						
-					else:
-						print("Буква не подходит")
-						ch2 += 1
-						if ch2 >= ch1:
-							print("Слово не подходит")
-							pass0 = ""
-							ch0 = 0
+    if vpa + vpA + vp0 + vpx == 0:
+        print('ERROR: no characters for password')
+        sys.exit()
 
 
-			if var1.get() == 1 and ch0 != 0 and ch1 > 2:
-				ch2 = 0
-				while ch2 < ch1:			
-					if pass0[ch2] in pA:
-						print("pA - совпадение")	 
-						ch2 += ch1*2
-						
-					else:
-						print("Буква не подходит")
-						ch2 += 1
-						if ch2 >= ch1:
-							print("Слово не подходит")
-							pass0 = ""
-							ch0 = 0
+    if vpa == 1:
+        M.extend(pa)
+    if vpA == 1:
+        M.extend(pA)
+    if vp0 == 1:
+        M.extend(p0)
+    if vpx == 1:
+        M.extend(px)
+    lenM = len(M)
 
+    spa = set(pa)
+    spA = set(pA)
+    sp0 = set(p0)
+    spx = set(px)
 
-			if var2.get() == 1 and ch0 != 0 and ch1 > 3:
-				ch2 = 0
-				while ch2 < ch1:			
-					if pass0[ch2] in p0:
-						print("p0 - совпадение")	 
-						ch2 += ch1*2
-						
-					else:
-						print("Буква не подходит")
-						ch2 += 1
-						if ch2 >= ch1:
-							print("Слово не подходит")
-							pass0 = ""
-							ch0 = 0
+    while ch0 <= var_len:
+        if ch0 == var_len:
 
+            spass0 = set(pass0)
 
-			if var3.get() == 1 and ch0 != 0 and ch1 > 4:
-				ch2 = 0
-				while ch2 < ch1:			
-					if pass0[ch2] in px:
-						print("pxS - совпадение")	 
-						ch2 += ch1*2
-						
-					else:
-						print("Буква не подходит")
-						ch2 += 1
-						if ch2 >= ch1:
-							print("Слово не подходит")
-							pass0 = ""
-							ch0 = 0
+            if vpa == 1 and var_len > 0:
+                if spass0 & spa == set():
+                    pass0 = ""
+                    ch0 = 0
 
+            if vpA == 1 and var_len > 1:
+                if spass0 & spA == set():
+                    pass0 = ""
+                    ch0 = 0
 
-			
-			if ch0 != 0:
-				ch0 = ch1+1
-		if ch0 < ch1:
-			rand0 = randrange(0, lenM, 1)
-			pass0 += str(M[rand0])
-			ch0 += 1
-	
-	ent_text1.set(pass0)
-	M = []
-	return 0
-	
+            if vp0 == 1 and var_len > 2:
+                if spass0 & sp0 == set():
+                    pass0 = ""
+                    ch0 = 0
+
+            if vpx == 1 and var_len > 3:
+                if spass0 & spx == set():
+                    pass0 = ""
+                    ch0 = 0
+
+            if ch0 != 0:
+                ch0 = var_len + 1
+        if ch0 < var_len:
+            rand0 = randrange(0, lenM, 1)
+            pass0 += str(M[rand0])
+            ch0 += 1
+
+    M = []
+    return pass0
+
 
 def _exit(event):
-	sys.exit()
-	
+    sys.exit()
 
-but_gen = Button(frame0, text="Generate", width=7)
-but_gen.bind("<Button-1>", gen)
-but_exit = Button(frame0, text='Exit', width=4)
-but_exit.bind("<Button-1>", _exit)
-
-
-cbut0 = Checkbutton(frame0,text="abc", variable=var0,onvalue=1,offvalue=0)
-cbut1 = Checkbutton(frame0,text="ABC", variable=var1,onvalue=1,offvalue=0)
-cbut2 = Checkbutton(frame0,text="012", variable=var2,onvalue=1,offvalue=0)
-cbut3 = Checkbutton(frame0,text="!@#", variable=var3,onvalue=1,offvalue=0)
-
-
-ent0 = Entry(frame0, width=5, bd=3, textvariable=ent_text0)
-ent1 = Entry(frame0, width=22, bd=3, textvariable=ent_text1)
-
-
-lab0 = Label(frame0, width=10, text="Длинна:", font="Sans 12")
-lab1 = Label(frame0, width=10, text="Символы:", font="Sans 12")
-lab2 = Label(frame0, width=10, text="Пароль:", font="Sans 12")
-
-
-frame0.grid(row=0, column=0)
-lab0.grid(row=0, column=0)
-ent0.grid(row=0, column=1)
-lab1.grid(row=1, column=0, columnspan=2)
-cbut0.grid(row=2, column=0)
-cbut1.grid(row=2, column=1)
-cbut2.grid(row=3, column=0)
-cbut3.grid(row=3, column=1)
-lab2.grid(row=7, column=0, columnspan=2)
-ent1.grid(row=8, column=0, columnspan=2)
-but_gen.grid(row=9, column=0)
-but_exit.grid(row=9, column=1)
 
 if __name__ == '__main__':
-	main()
-
-root.mainloop()
+    main()
+else:
+    print('import module passgen')
